f3f4c7f908d64a69ae2d44ce90f0c6914c68880a
u <mjkim2304@gmail.com>
2022-11-23 14:28:16 +0900

    Author과 Book 테이블을 join해서 보여주는 API

---
+++ src/main/java/com/jpa/exercise/repository/AuthorRepository.java
@@ -0,0 +1,6 @@
+package com.jpa.exercise.repository;
+
+import com.jpa.exercise.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {}
---
+++ src/main/java/com/jpa/exercise/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.jpa.exercise.repository;
+
+import com.jpa.exercise.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {}
